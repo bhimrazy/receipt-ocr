3708983da2b7a72254628e821c39563abd1565e0
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2024-07-05 06:25:51 +0000

    ref: format

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
         raise Exception("The given image does not exist.")
 
     # load the image, resize and compute ratio
-    img_orig  = cv2.imread(args.image)
+    img_orig = cv2.imread(args.image)
     image = img_orig.copy()
     image = imutils.resize(image, width=500)
     ratio = img_orig.shape[1] / float(image.shape[1])
@@ -27,14 +27,20 @@ def main():
     # convert the image to grayscale, blur it slightly, and then apply
     # edge detection
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (5, 5,), 0)
+    blurred = cv2.GaussianBlur(
+        gray,
+        (
+            5,
+            5,
+        ),
+        0,
+    )
     edged = cv2.Canny(blurred, 75, 200)
-
+    # cv2.imwrite("edged.jpg", edged)
 
     # find contours in the edge map and sort them by size in descending
     # order
-    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
 
@@ -50,22 +56,26 @@ def main():
         if len(approx) == 4:
             receiptCnt = approx
             break
-    
+
     # cv2.drawContours(image, [receiptCnt], -1, (0, 255, 0), 2)
+    # cv2.imwrite('image_with_outline.jpg', image)
     # cv2.imshow("Receipt Outline", image)
     # cv2.waitKey(0)
-    
+
     # if the receipt contour is empty then our script could not find the
     # outline and we should be notified
     if receiptCnt is None:
-        raise Exception(("Could not find receipt outline. "
-            "Try debugging your edge detection and contour steps."))
-
+        raise Exception(
+            (
+                "Could not find receipt outline. "
+                "Try debugging your edge detection and contour steps."
+            )
+        )
 
     # apply a four-point perspective transform to the *original* image to
     # obtain a top-down bird's-eye view of the receipt
     receipt = four_point_transform(img_orig, receiptCnt.reshape(4, 2) * ratio)
-
+    # cv2.imwrite('transformed_receipt.jpg', receipt)
 
     # apply OCR to the receipt image by assuming column data, ensuring
     # the text is *concatenated across the row* (additionally, for your
@@ -73,13 +83,14 @@ def main():
     # the image, including resizing, thresholding, etc.)
     options = "--psm 6"
     text = pytesseract.image_to_string(
-        cv2.cvtColor(receipt, cv2.COLOR_BGR2RGB),
-        config=options)
+        cv2.cvtColor(receipt, cv2.COLOR_BGR2RGB), config=options
+    )
     # show the raw output of the OCR process
     print("[INFO] raw output:")
     print("==================")
     print(text)
     print("\n")
+
 
 if __name__ == "__main__":
     main()
